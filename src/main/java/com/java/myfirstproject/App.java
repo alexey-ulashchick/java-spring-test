03c8b96647c13cca2ea1e9243c2cd8dd6682ba82
Alexey Ulashchick <a.ulashchick@gmail.com>
2019-09-25 19:45:16 -0700

    First commit

---
+++ src/main/java/com/java/myfirstproject/App.java
@@ -0,0 +1,20 @@
+package com.java.myfirstproject;
+
+import java.util.Collections;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(App.class);
+        app.setDefaultProperties(Collections.singletonMap("server.port", "8083"));
+        app.run(args);
+    }
+
+}
